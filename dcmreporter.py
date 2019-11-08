792fb10abbee69607f4b9155642f6ad455d451b3
Siddhartha Dhiman <sdhiman@buffalo.edu>
2019-11-08 12:02:17 -0500

    Filename changes

---
+++ dcmreporter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import textwrap
+import os.path as op
+from studyreport import studyreport as sr
+
+parser = argparse.ArgumentParser(
+        prog='studyreport',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''\
+DICOM Study Report
+------------------
+This is a heuristic tool to generate a study report based on DICOM files in
+a study directory. A recursive search is performed on all dicom files 
+in a folder and analyzed for important information, which is then condensed
+into a Pandas Datafram and output as a .csv file.
+                                '''))
+# Mandatory
+parser.add_argument('input',
+                    help='Path to study folder containing sorted dicom '
+                         'folders',
+                    type=str)
+
+parser.add_argument('output',
+                    help='Output location. '
+                    'Default: same path as dwi.',
+                    type=str)
+# Optional
+parser.add_argument('-r' , '--report',
+                    action='store',
+                    help='Generates a study report of DICOM files')
+parser.add_argument('-c' , '--convert',
+                    action='store',
+                    help='Converts DICOMs to NifTis (Work in Progress')
+parser.add_argument('-n', '--nthreads',
+                    action='store',
+                    help='Number of workers to process with. Default: 1')
+args = parser.parse_args()
+
+# Perform Checks
+if not op.exists(args.input):
+    raise Exception('Study directory does not exist. Please ensure that '
+                    'the directory provided exists')
+if not args.output:
+    raise Exception('Please provide an output filename (.csv) or '
+                    'directory')
+if not args.nthreads:
+    args.nthreads = 1
+else:
+    args.nthreads=int(args.nthreads)
+
+# Run Script
+study = sr(args.input,
+           nthreads=args.nthreads)
+study.getstudyinfo()
+table = study.createstudytable()
+study.writepdtable(table, args.output)
--- studyreport.py
+++ studyreport.py
@@ -1,59 +1,260 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import argparse
-import textwrap
-import os.path as op
-from dcmreport import studyreport as sr
+from os import path as op
+import pandas as pd
+import numpy as np
+import pydicom as pyd
+import glob
+from tqdm import tqdm, trange
+import multiprocessing
+from joblib import Parallel, delayed
 
-parser = argparse.ArgumentParser(
-        prog='studyreport',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent('''\
-DICOM Study Report
-------------------
-This is a heuristic tool to generate a study report based on DICOM files in
-a study directory. A recursive search is performed on all dicom files 
-in a folder and analyzed for important information, which is then condensed
-into a Pandas Datafram and output as a .csv file.
-                                '''))
-# Mandatory
-parser.add_argument('input',
-                    help='Path to study folder containing sorted dicom '
-                         'folders',
-                    type=str)
+tqdmwidth = 70
 
-parser.add_argument('output',
-                    help='Output location. '
-                    'Default: same path as dwi.',
-                    type=str)
-# Optional
-parser.add_argument('-r' , '--report',
-                    action='store',
-                    help='Generates a study report of DICOM files')
-parser.add_argument('-c' , '--convert',
-                    action='store',
-                    help='Converts DICOMs to NifTis (Work in Progress')
-parser.add_argument('-n', '--nthreads',
-                    action='store',
-                    help='Number of workers to process with. Default: 1')
-args = parser.parse_args()
+class studyreport():
+    """
+    The study report class object computes and holds important study
+    metrics for a dicom-based study. It serves as a means to estimate
+    study health based on a dicom headers.
 
-# Perform Checks
-if not op.exists(args.input):
-    raise Exception('Study directory does not exist. Please ensure that '
-                    'the directory provided exists')
-if not args.output:
-    raise Exception('Please provide an output filename (.csv) or '
-                    'directory')
-if not args.nthreads:
-    args.nthreads = 1
-else:
-    args.nthreads=int(args.nthreads)
+    Parameters
+    ----------
+    studydir:   str
+                path to study directory containing dicom files
 
-# Run Script
-study = sr(args.input,
-           nthreads=args.nthreads)
-study.getstudyinfo()
-table = study.createstudytable()
-study.writepdtable(table, args.output)
+    Methods
+    -------
+    getsubid:       Returns patient ID from the 'PatientName' field of a
+                    DICOM header
+
+    get sublist:
+    """
+    def __init__(self, studydir, nthreads=1):
+        self.flist = np.array(glob.glob(op.join(studydir, '**/*.dcm'),
+                           recursive=True))
+        if not isinstance(nthreads, int):
+            raise Exception('Variable nthreads need to be an integer')
+        if nthreads < -1 or nthreads == 0:
+            raise Exception('Variable nthreads is a positive integer or '
+                            '-1')
+        if nthreads is None:
+            self.workers = -1
+        else:
+            self.workers = nthreads
+        if self.workers == -1:
+            tqdm.write('Processing with ' +
+                       np.str(multiprocessing.cpu_count()) +
+                       ' workers...')
+        else:
+            tqdm.write('Processing with ' +
+                       np.str(self.workers) +
+                       ' workers...')
+
+    def getstudyinfo(self):
+        """
+        Reads off varios DICOM header fields and returns a Pandas
+        Dataframe on information discovered in a the class object
+        self.dicomprops
+
+        Parameters
+        ----------
+        (none)
+
+        Returns
+        -------
+        self.dicomprops:    Pandas Dataframe
+                            Contaions DICOM information per subject
+        """
+        inputs = trange(len(self.flist),
+                        desc='Analyzing Files',
+                        unit='files',
+                        ncols=tqdmwidth)
+        infoarray = Parallel(self.workers, prefer='processes') \
+            (delayed(self.getsubfield)(self.flist[i]) for i in inputs)
+        self.dicomprops = pd.DataFrame(infoarray,
+                                      columns=['PatientID',
+                                               'AcquisitionDate',
+                                               'PatientSex',
+                                               'PatientAge',
+                                               'SoftwareVersion',
+                                               'ProtocolName',
+                                               'SequenceName'])
+
+    def subtablehelper(self, subject, protolist):
+        """
+        This is a helper function to make class method createstudytable()
+        compatible with joblib parallel processing. This function outputs
+        a list containins sums of dicom files per protocol for a given
+        subject.
+
+        Parameters
+        ----------
+        subject:    string
+                    SubjectID of subject to compute sums of
+        protolist:  string list
+                    list of protocols to query for sum
+
+        Returns
+        -------
+        protosum:   int list
+                    list containing sum of files in queried protocols
+        """
+        subchunk = self.dicomprops.loc[self.dicomprops.loc[:,
+                                       'PatientID'] == subject, :]
+        protosum = []
+        protosum.append(subchunk.loc[:, 'AcquisitionDate'].iloc[0])
+        protosum.append(subchunk.loc[:, 'PatientSex'].iloc[0])
+        protosum.append(subchunk.loc[:, 'PatientAge'].iloc[0])
+        protosum.append(subchunk.loc[:, 'SoftwareVersion'].iloc[0])
+        for i, proto in enumerate(protolist):
+            protosum.append(np.sum(
+                subchunk.loc[:, 'ProtocolName'] == proto))
+        return protosum
+
+    def createstudytable(self):
+        """
+        Returns a condensed Pandas Dataframe from the master dataframe
+        returned by class method getstudyinfo(). The condensed dataframe
+        returned here containins sum of all files per protocol per subject
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        studytable: Pandas Dataframe
+                    Contains sum of all files in a protocol, per subject
+        """
+        sublist = np.sort(self.makeunique('PatientID'))
+        protolist = np.sort(self.makeunique('ProtocolName'))[::-1]
+        protolist = list(filter(None, protolist))
+        inputs = trange(len(sublist),
+                        desc='Creating Dataframe',
+                        unit='sub',
+                        ncols=tqdmwidth)
+        studytable = Parallel(n_jobs=self.workers, prefer='processes') \
+            (delayed(self.subtablehelper)(sublist[i], protolist) for i in
+             inputs)
+        newcols = ['AcquisitionDate', 'PatientSex', 'PatientAge',
+                   'SoftwareVersion']
+        newcols.extend(protolist)
+        studytable = pd.DataFrame(studytable,
+                                  index=sublist,
+                                  columns=newcols)
+        return studytable
+
+    def writepdtable(self, pdtable, outdir):
+        '''
+        Writes a Pandas Dataframe onto disk as a CSV file. Include
+        filename with .csv extension here.
+
+        Parameters
+        ----------
+        pdtable:    Pandas Dataframe
+                    Table to write
+        outdir:     Path to write to
+        '''
+        if outdir is None:
+            raise Exception('Please specify and output directory')
+        ext = list(op.splitext(outdir))
+        ext[1] = '.csv'
+        outdir = ext[0] + ext[1]
+        pdtable.to_csv(outdir)
+        print('File saved to ' + outdir)
+
+    def getsublist(self):
+        '''
+        Returns a list of all subjects discovered in a study
+
+        Parameters
+        ----------
+        (none)
+
+        Returns
+        -------
+        sublist:    list of strings
+                    contains all subjects discovered
+        '''
+        sublist = pd.unique(self.dicomprops.loc[:, 'PatientID'])
+        tqdm.write('Discovered '+ str(len(sublist)) + ' subjects')
+        return sublist
+
+    def makeunique(self, colidx=0):
+        """
+        Makes a numpy array unique based on an input column index
+
+        Parameters
+        ----------
+        colidx: int or str
+                column index according which to make unique. This is a
+                Pandas column index, so use either an integer or string
+
+        Returns
+        -------
+        uniquevec:  Numpy array of strings
+                    Numpy array containing unique list
+        """
+        if not (isinstance(colidx, int) or  isinstance(colidx, str)):
+            raise Exception('Column index is not a supported variable '
+                            'type. Please use either an integer or '
+                            'string and ensure it is a valid Pandas '
+                            'dataframe index')
+
+        if isinstance(colidx, int):
+            if colidx < 0:
+                raise Exception(
+                    'Column index needs to be a positive integer')
+            else:
+                uniquevec = pd.unique(self.dicomprops.iloc[:, colidx])
+        elif isinstance(colidx, str):
+            uniquevec = pd.unique(self.dicomprops.loc[:, colidx])
+        return uniquevec
+
+    def getsubfield(self, dcmpath):
+        """
+        Returns a numpy array containing essential information for an
+        input dicom file
+
+        Parameters
+        ----------
+        dcmpath:    string
+            path to dicom file (.dcm)
+
+        Returns
+        -------
+        subinfo:    list of strings
+                    List containing subject information
+        """
+        subinfo = []
+        dcmfile = pyd.dcmread(dcmpath)
+        subinfo.append(dcmfile.PatientID)
+        try:
+            subinfo.append(dcmfile.AcquisitionDate)
+        except:
+            subinfo.append('N/A')
+        subinfo.append(dcmfile.PatientSex)
+        subinfo.append(dcmfile.PatientAge)
+        subinfo.append(dcmfile.SoftwareVersions)
+        subinfo.append(dcmfile.ProtocolName)
+        try:
+            subinfo.append(dcmfile.SequenceName)
+        except:
+            subinfo.append('N/A')
+        return subinfo
+
+    def getsubid(self, dcmpath):
+        """
+        Returns patient ID from the 'PatientID' field of a DICOM header
+
+        Parameters
+        ----------
+        dcmpath:    string
+            path to dicom file (.dcm)
+
+        Returns
+        -------
+        Patient ID in string
+        """
+        dcmfile = pyd.dcmread(dcmpath)
+        return dcmfile.PatientID
